dfb1034c0ea437660dce050701dcee2c5c1747cb
Li Zhaoyang <lizy14@yeah.net>
2015-10-12 16:52:49 +0800

    warshall

--- graph/Warshall-algorithm/main.cpp
+++ graph/Warshall-algorithm/main.cpp
@@ -11,41 +11,78 @@
 
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+
+#include <stdlib.h>
+
 
 class Graph{
-public:
-    int **matrix; //adjacency matrix
+    int **matrixAdjacency; //邻接矩阵
+	int **matrixD; //最短路径权重矩阵
     int nEdges, nVertexes;
 public:
-    static const int NO_PATH = -1;
+    static const int INFINITY = 1000000/2;
 public:
     void read(std::istream&);
     int lengthOfShortestPath(int vertexA, int vertexB);
+	void warshall();
     void printMatrix(std::ostream&);
     Graph():
-        matrix(NULL),
+        matrixAdjacency(NULL),
+		matrixD(NULL),
         nEdges(0),
         nVertexes(0){}
     ~Graph();
 };
+void Graph::warshall(){
+	//make an n*n matrix
+	matrixD = new int*[nVertexes];
+	for(int i=0; i<nVertexes; i++){
+		matrixD[i] = new int[nVertexes];
+		for(int j=0; j<nVertexes; j++)
+			if(i==j)
+				matrixD[i][j]=0;
+			else if(matrixAdjacency[i][j])
+				matrixD[i][j]=matrixAdjacency[i][j];
+			else
+				matrixD[i][j]=INFINITY;
+	}
+
+	//here we go
+	int a, b;
+	for(int k=0; k<nVertexes; k++){
+		for(int j=0; j<nVertexes; j++){
+			for(int i=0; i<nVertexes; i++){
+					a = matrixD[i][j];
+					b = matrixD[i][k]+matrixD[k][j];
+					matrixD[i][j] = std::min(a, b);
+			}
+		}
+	}
+	return;
+}
 int Graph::lengthOfShortestPath(int vertexA, int vertexB){
-    if(vertexA == vertexB)
-        return 0;
-    //TODO: 
-    return NO_PATH;
+    return matrixD[vertexA][vertexB];
 }
 Graph::~Graph(){
-    if(matrix){
+    if(matrixAdjacency){
         for(int i=0; i<nVertexes; i++){
-            delete[] matrix[i];
+            delete[] matrixAdjacency[i];
         }
-        delete[] matrix;
+        delete[] matrixAdjacency;
     }
+	if(matrixD){
+		for(int i=0; i<nVertexes; i++){
+			delete[] matrixD[i];
+		}
+		delete[] matrixD;
+	}
 }
 void Graph::printMatrix(std::ostream& s){
+	s << "Matrix" << std::endl;
     for(int i=0; i<nVertexes; i++){
         for(int j=0; j<nVertexes; j++){
-            s << matrix[i][j] <<' ';
+            s << matrixD[i][j] <<' ';
         }
         s << std::endl;
     }
@@ -53,30 +90,30 @@ void Graph::printMatrix(std::ostream& s){
 void Graph::read(std::istream& s){
     s >> nVertexes >> nEdges;
     
-    //make matrix a n*n array
-    matrix = new int*[nVertexes];
+    //make an n*n matrix
+    matrixAdjacency = new int*[nVertexes];
     for(int i=0; i<nVertexes; i++){
-        matrix[i] = new int[nVertexes];
+        matrixAdjacency[i] = new int[nVertexes];
 		for(int j=0; j<nVertexes; j++)
-			matrix[i][j]=0;
+			matrixAdjacency[i][j]=0;
     }
     
     int _1, _2, _3;
     for(int i=0; i<nEdges; i++){
         s >> _1 >> _2 >> _3;
-        matrix[_1-1][_2-1] = _3;
+        matrixAdjacency[_1-1][_2-1] = _3;
     }
 }
 int test(std::istream& s){
     Graph graph;
     graph.read(s);
-    
+    graph.warshall();
     int _, _1, _2, __;
 	s >> _;
     for(int i=0; i<_; i++){
         s >> _1 >> _2;
         __ = graph.lengthOfShortestPath(_1-1, _2-1);
-        if(__ == Graph::NO_PATH)
+        if(__ >= Graph::INFINITY)
             std::cout << "NO PATH" << std::endl;
         else
             std::cout << __ << std::endl;
