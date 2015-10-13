5e2830ec101a10684ad25539ba3decb1e825cddf
Li Zhaoyang <lizy14@yeah.net>
2015-10-13 18:08:19 +0800

    problem 3.22

--- stack-and-queue/main.cpp
+++ stack-and-queue/main.cpp
@@ -12,6 +12,7 @@
 
 #include <stack>
 #include <queue>
+#include <vector>
 #include <iostream>
 #include <string>
 
@@ -108,6 +109,166 @@ bool isPalindrome(
 	}
 }
 
+
+//evaluate a string consists only of integers and binary operators +-*/
+int priority(const char chr){
+	if(chr == '+' || chr == '-')
+		return 10;
+	if(chr == '*' || chr == '/')
+		return 20;
+	if(chr == '#')
+		return 1;
+	return 0;
+}
+class Item{
+public:
+	enum Type{EMTPY, OPERATOR, OPERAND} type;
+	union{
+		char chr;
+		double dbl;
+	};
+public:
+	Item(char c):chr(c),type(OPERATOR){}
+	Item(double d):dbl(d),type(OPERAND){}
+	operator double(){
+		if(type!=OPERAND)
+			throw "Item type error";
+		return dbl;
+	}
+	operator char(){
+		if(type!=OPERATOR)
+			throw "Item type error";
+		return chr;
+	}
+	void clear(){type=EMTPY;}
+};
+class SuffixExpression : public std::vector<Item>{
+	//nothing here
+};
+void printExpression(const SuffixExpression suffixExpression){
+	for(Item item : suffixExpression){
+		if(item.type == Item::OPERAND)
+			std::cout << (double)item << ' ';
+		else if(item.type == Item::OPERATOR)
+			std::cout << (char)item << ' ';
+	}
+	std::cout << '\n';
+}
+double evaluateExpression(SuffixExpression expression){
+	
+	int length = expression.size();
+	if(length == 2)
+		return expression[0];
+	int i=0;
+	while(i<length-1){//ignore the ending '#'
+
+#ifdef LOCAL_DEBUG
+		printExpression(expression);
+#endif
+		while(expression[i].type != Item::OPERATOR){
+			i++;
+		}
+		//found an operator
+		int positionOfOperator = i;
+		double a, b;
+		i--;
+		while(expression[i].type != Item::OPERAND){
+			i--;
+		}
+		a = expression[i];
+		expression[i].clear();
+		i--;
+		while(expression[i].type != Item::OPERAND){
+			i--;
+		}
+		b = expression[i];
+		expression[i].clear();
+
+		//now operands found
+		double result;
+		switch(expression[positionOfOperator]){
+		case '+':
+			result = b+a;
+			break;
+		case '-':
+			result = b-a;
+			break;
+		case '*':
+			result = b*a;
+			break;
+		case '/':
+			result = b/a;
+			break;
+		default:
+			throw "unknown operator";
+		}
+
+		expression[positionOfOperator] = result;
+		i = positionOfOperator + 1;
+	}
+	return expression[length-2];
+}
+SuffixExpression makeSuffixExpression(std::string expression){
+	expression = expression + "#";	
+	double current = 0;
+	double dot = 0.;
+	
+	std::stack<char> operators;
+	operators.push('#');
+
+	SuffixExpression suffixExpression;
+
+	
+	for(char chr : expression){
+		
+
+		if(('0'<=chr && chr<='9') || chr=='.'){
+			//got a digit
+			if(chr=='.'){
+				if(dot)
+					throw "invalid decimal point";
+				dot = 1.;
+			}else{
+				if(dot){
+					dot /= 10;
+					current += dot * (chr - '0');
+				}else{
+					current *= 10;
+					current += (chr - '0');
+				}
+			}
+		}else{
+			//got an operator
+
+			//take care of something concerning reading digits
+			suffixExpression.push_back(current);
+
+			current = 0.;
+			dot = 0.;
+
+			//now deal with `chr`
+			if(!priority(chr))
+				throw "unknown operator";
+
+
+			char top = operators.top();
+			if(priority(chr) > priority(top)){
+				operators.push(chr);
+			}else{
+				while(priority(top) >= priority(chr)){
+					suffixExpression.push_back(top);
+					operators.pop();
+					if(operators.empty())
+						break;
+					top = operators.top();	
+				}
+				operators.push(chr);
+			}
+		}
+	}
+	return suffixExpression;
+}
+
 void printBool(bool b){
 	std::cout << (b? "true": "false") << std::endl;
 }
@@ -122,7 +283,15 @@ void problem3_19(){
 	printBool(answer);
 }
 void problem3_22(){
-
+	std::string str;
+#ifdef LOCAL_DEBUG
+	while(1){
+#endif
+		std::getline(std::cin, str);
+		std::cout << evaluateExpression(makeSuffixExpression(str)) << std::endl;
+#ifdef LOCAL_DEBUG
+	}
+#endif
 }
 void problem3_28(){
 
@@ -135,7 +304,7 @@ void problem3_32(){
 
 }
 int main(){
-	problem3_17();
+	problem3_22();
 #ifdef LOCAL_DEBUG
 	system("pause");
 #endif
