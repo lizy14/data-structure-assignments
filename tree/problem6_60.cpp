d6809284f9358878a16a6a1b3f22725c80d035f2
Li Zhaoyang <lizy14@yeah.net>
2015-11-13 08:02:51 +0000

    problem 6_60 AC

--- tree/problem6_60.cpp
+++ tree/problem6_60.cpp
@@ -1,6 +1,6 @@
 /*
 文件名: 
-描　述: 数据结构习题习题6.59：无重复地输出以孩子兄弟链表存储的树中所有的边
+描　述: 数据结构习题习题6.60：统计孩子-兄弟链表存储的树T中叶子的个数
 
 作　者: 李肇阳, 清华大学软件学院, lizy14@yeah.net
 创建于: 2015-11-13
@@ -32,6 +32,7 @@ CSTree is an array of CSNode, uses subscript 1 to n
 &operator[](1) points to the root node.
 number of nodes saved in operator[](0).data
 */
+
 CSTree createTree(){
     int n;
     cin >> n;
@@ -54,42 +55,18 @@ void destroyTree(CSTree tree){
     delete[] tree;
 }
 
-
-//to be called recursively.
-void printEdgesOfSubTree(CSNode *root, Edges& edges, int depth){
-    if(root==nullptr)
-        return;
-    CSNode *p;
-    p = root->firstchild;
-    while(p!=nullptr){
-        if(edges.size()<depth+1)
-            edges.resize(depth+1);
-        edges[depth].push_back(Edge(root->data, p->data));
-        p = p->nextsibling;
+int countLeafNodes(CSTree tree){
+    int n=tree[0].data;
+    int count = 0;
+    for(int i=1; i<n; i++){
+        if(tree[i].firstchild==nullptr)
+            count ++;
     }
-    p = root->firstchild;
-    while(p!=nullptr){
-        printEdgesOfSubTree(p, edges, depth+1);
-        p = p->nextsibling;
-    }
-    
+    return count;
 }
-
-void printEdges(CSTree tree){
-    if(tree==nullptr)
-        return;
-    Edges edges;
-    printEdgesOfSubTree(&tree[1], edges, 0);
-    for(int i=0; i<edges.size(); i++){
-        for(int j=0; j<edges[i].size(); j++){
-            cout << edges[i][j].a << ' ' <<  edges[i][j].b << '\n';
-        }
-    }
-}
-
 int main(){
     CSTree tree = createTree();
-    printEdges(tree);
+    cout << countLeafNodes(tree);
     destroyTree(tree);
 #ifdef _DEBUG
     system("pause");
