09deba48e4f8994a4e720f8b1626e7932267dd31
Li Zhaoyang <lizy14@yeah.net>
2015-11-15 07:53:03 +0000

    Dijkstra-algorithm AC

--- graph/Dijkstra-algorithm/main.cpp
+++ graph/Dijkstra-algorithm/main.cpp
@@ -1,4 +1,3 @@
-#define _DEBUG
 /*
 文件名: 
 描　述: Dijkstra 算法求正权有向图中某点到其他点之间的最短路径
@@ -13,32 +12,108 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <set>
+#include <stack>
 
 class Graph{
-    int **matrixAdjacency; //邻接矩阵
+    typedef int weight;
+    typedef int nodeID;
+    std::vector<std::vector<weight> > matrixAdjacency; //邻接矩阵
+    std::vector<nodeID> Q;//单源最短路径信息
+    std::vector<weight> pi;//单源最短路径的长度
+    nodeID startingPoint;
     int nEdges, nVertexes;
 public:
-    static const int INFINITY = 1000000/2;
+    static const weight INFINITY = 1000000/2;
 public:
     void read(std::istream&);
-	void dijkstra(int);
     void printMatrix(std::ostream&);
+	
+    void dijkstra(nodeID);
+    weight getPathLength(nodeID destination){
+        if(destination==startingPoint)
+            return 0;
+        return pi[destination];
+    }
+    std::vector<nodeID> getPath(nodeID destination){
+        std::vector<nodeID> vector;
+        std::stack<nodeID> stack;
+        
+        nodeID p = destination;
+        while(Q[p]!=startingPoint){
+           stack.push(Q[p]);
+           p = Q[p];
+        }
+        while(!stack.empty()){
+            vector.push_back(stack.top());
+            stack.pop();
+        }
+        return vector;
+    }
+    
+    weight& getWeight(nodeID start, nodeID end){
+        return matrixAdjacency[start][end];
+    }
     Graph():
         matrixAdjacency(NULL),
         nEdges(0),
         nVertexes(0){}
-    ~Graph();
+    ~Graph(){}
 };
-void Graph::dijkstra(int startingPoint){
-	return;
-}
-Graph::~Graph(){
-    if(matrixAdjacency){
-        for(int i=0; i<nVertexes; i++){
-            delete[] matrixAdjacency[i];
-        }
-        delete[] matrixAdjacency;
+void Graph::dijkstra(nodeID startingPoint_){
+    startingPoint = startingPoint_;
+
+    std::set<nodeID> T;
+    for(int i=0; i<nVertexes; i++){
+        if(i!=startingPoint)
+            T.insert(i);
     }
+
+    pi.resize(nVertexes);
+    for(int i=0; i<nVertexes; i++){
+        pi[i]=getWeight(startingPoint,i);
+    }
+
+    Q.resize(nVertexes, startingPoint);
+
+    //initialization done. here comes the main loop
+
+    while(T.size()!=0){
+        //find the node inside T with minimal pi
+        int minPi = INFINITY;
+        int minID = *T.begin();
+        
+        //for(auto& i: T){
+        //fuck the OJ platform not supporting  `--std=c++11`
+        for(
+            std::set<nodeID>::iterator j=T.begin(); 
+            j!=T.end(); j++){
+            nodeID i = (*j);
+            if(pi[i]<minPi){
+                minID = i;
+                minPi = pi[i];
+            }
+        }
+
+        //found it.
+        T.erase(T.find(minID));
+        
+
+        //modify T and Q
+        //for(auto& i: T){
+        for(
+            std::set<nodeID>::iterator j=T.begin(); 
+            j!=T.end(); j++){
+            nodeID i = (*j);
+            weight possibleNewPi = pi[minID] + getWeight(minID, i);
+            if(possibleNewPi < pi[i]){
+                pi[i]=possibleNewPi;
+                Q[i] = minID;
+            }
+        }
+    }
+
+	return;
 }
 void Graph::printMatrix(std::ostream& s){
 	s << "Matrix" << std::endl;
@@ -53,11 +128,11 @@ void Graph::read(std::istream& s){
     s >> nVertexes >> nEdges;
     
     //make an n*n matrix
-    matrixAdjacency = new int*[nVertexes];
-    for(int i=0; i<nVertexes; i++){
-        matrixAdjacency[i] = new int[nVertexes];
-		for(int j=0; j<nVertexes; j++)
-			matrixAdjacency[i][j]=0;
+    matrixAdjacency.resize(nVertexes);
+
+    //for(auto& i : matrixAdjacency){
+    for(int j=0; j<nVertexes; j++){
+        matrixAdjacency[j].resize(nVertexes, INFINITY);
     }
     
     int _1, _2, _3;
@@ -69,22 +144,34 @@ void Graph::read(std::istream& s){
 int test(std::istream& s){
     Graph graph;
     graph.read(s);
-    graph.printMatrix(std::cout);
+
     int n, startingPoint, endingPoint;
 	s >> n >> startingPoint;
+
     graph.dijkstra(startingPoint);
     for(int i=0; i<n; i++){
         s >> endingPoint;
-        //output here
+        int length = graph.getPathLength(endingPoint);
+        if(length>=Graph::INFINITY){
+            std::cout << "NO PATH\n";
+            continue;
+        }
+        std::cout << startingPoint << "-";
+        std::vector<int> path = graph.getPath(endingPoint);
+        for(int i=0; i<path.size(); i++){
+            std::cout << path[i] << '-';
+        }
+        std::cout << endingPoint << ' ' << length << '\n';
     }
     return 0;
 }
 int main(){
-    //for online judge
 #ifndef _DEBUG
+    //for online judge
     return test(std::cin);
-#endif
+#else
     //for local debug
     std::ifstream file("input.txt");
-    return test(file);
+    return test(file) || system("pause");
+#endif
 }
